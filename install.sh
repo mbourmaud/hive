86c40373399073adc9de3490535c1d12f14b1196
Hive Drone <drone@hive.local>
2026-02-12 15:31:47 +0100

    chore: update install script to v3.5.0 [skip ci]

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ fi
 
 REPO="mbourmaud/hive"
 # This version is automatically updated by CI when a release is fully built
-STABLE_VERSION="3.4.0"
+STABLE_VERSION="3.5.0"
 
 echo ""
 echo -e "${YELLOW}"
